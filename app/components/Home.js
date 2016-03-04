5ca0811a4eb70c76e40e0f110d687a284ebc664f
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-04 15:03:30 -0600

    setup server side rendering

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = React.createClass({
+  render: {
+    return(
+      <div className='home'>
+        Hello from Home!
+      </div>
+    )
+  }
+})
+
+export default Home
