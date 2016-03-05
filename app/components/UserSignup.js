8f340dd08c9919640a3190d8bab1866b00608f7f
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-05 16:00:11 -0600

    added signup components

---
+++ app/components/UserSignup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class UserSignup extends React.Component {
+  render() {
+      return (
+        <div className='home' >
+          <div className="admin">
+            <button type="submit" >
+              Admin
+            </button>
+          </div>
+          <div className='Hobo'>
+            <button type="submit" >
+              Hobo
+            </button>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default UserSignup;
