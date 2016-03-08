64482064af2b49fbd7b0196d58936241a7b155c1
Carly Levy <carlylevy@gmail.com>
2016-03-08 15:27:15 -0600

    refactored to include shelterid and error handling closes #85

--- server/dbHelpers/shelters.js
+++ server/dbHelpers/shelters.js
@@ -10,6 +10,7 @@ var getOrgID = function(orgName){
               .where('organizationName', orgName)
   .catch(function(err){
     console.log("Could not find organization with this name.", err);
+    throw new Error("Could not find organization with this name", err);
   })
   .then(function(orgID){
     console.log("Found organization with id ", orgID[0].organizationID);
@@ -30,85 +31,83 @@ module.exports.insertShelter = function(req){
     var email = req.shelters.shelterEmail;
     var emergencyPhone = req.shelters.shelterEmergencyPhone;
     var daytimePhone = req.shelters.shelterDayTimePhone;
-    console.log("lookup this org name", req.organizations.orgName);
   
     return getOrgID(req.organizations.orgName)
       .then(function(org){
         console.log("result from look up org: ", org);
         return knex('shelters')
-              .insert({
-                  shelterName: name, 
-                  shelterEmail: email, 
-                  shelterEmergencyPhone: emergencyPhone, 
-                  shelterDaytimePhone: daytimePhone,
-                  fk_organizationID: org
-                  // fk_shelterlocationID: shelterLocation,
-                  // fk_hoursID: shelterHours
-                })
-              .returning('*')
-              .catch(function(err){
-                console.log("Something went wrong inserting this shelter ", err);
-              })
-              .then(function(shelter){
-                console.log('Successfully inserted shelter with ID ', shelter[0].shelterID);
-                return shelter;
-              });     
+                .insert({
+                    shelterName: name, 
+                    shelterEmail: email, 
+                    shelterEmergencyPhone: emergencyPhone, 
+                    shelterDaytimePhone: daytimePhone,
+                    fk_organizationID: org
+                    // fk_shelterlocationID: shelterLocation,
+                    // fk_hoursID: shelterHours
+                  })
+                .returning('*')
+          .catch(function(err){
+            console.log("Something went wrong inserting this shelter ", err);
+            throw new Error("Something went wrong inserting this shelter ", err);
+          })
+          .then(function(shelter){
+            console.log('Successfully inserted shelter with ID ', shelter[0].shelterID);
+            return shelter;
+          });     
       });
 
 };
 
 var getShelterID = function(shelterName){
-      console.log('passed in shelter name ', shelterName);
-
       return knex.select('shelterID').from('shelters')
-              .where('shelterName', shelterName)
-              .catch(function(err){
-                console.log("Something went wrong. This shelter may not exist.");
-              })
-              .then(function(shelterID){
-                console.log("Returing shelterID ", shelterID);
-                return shelterID[0].shelterID;
-              });
+          .where('shelterName', shelterName)
+          .catch(function(err){
+            console.log("Something went wrong. This shelter may not exist.");
+            throw new Error("Something went wrong. This shelter may not exist.");
+          })
+          .then(function(shelterID){
+            console.log("Returing shelterID ", shelterID);
+            return shelterID;
+          });
 
 };
 
 module.exports.selectShelter = function(req){
-      var shelter = req.shelters;
-        console.log("Passed in shelter with name", shelter);
-
-      return getShelterID(shelter)
-            .then(function(shelterID){
-              return knex.select('*').from('shelters')
-                        .where('shelterID', shelterID)
-                .catch(function(err){
-                  console.log("Something went wrong selecting this shelter ", err);
-                })
-                .then(function(shelter){
-                console.log("returned from select shelter : " , shelter);
-                  return shelter;
-                });
-            });
+    var shelter = req.shelters;
+    return getShelterID(shelter)
+      .then(function(shelter){
+        var shelterID = shelter[0].shelterID;
+        return knex.select('*').from('shelters')
+                  .where('shelterID', shelterID)
+          .catch(function(err){
+            console.log("Something went wrong selecting this shelter ", err);
+            throw new Error("Something went wrong selecting this shelter", err);           
+          })
+          .then(function(shelter){
+          console.log("returned from select shelter : " , shelter);
+            return shelter;
+          });
+      });
         //function for selecting shelter units
 };
 
 module.exports.updateShelter = function(req){
   //function for updating shelter units
-  // var shelterName = req.shelters.shelterName;
-    // var thisShelterID = getShelterID(req.shelters.shelterName);
     var name = req.shelters.shelterName;
     var email = req.shelters.shelterEmail;
     var emergencyPhone = req.shelters.shelterEmergencyPhone;
     var daytimePhone = req.shelters.shelterDayTimePhone;
     return knex('shelters')
-        .where('shelterID', thisShelterID)
-        .update({
-            shelterName: name,
-            shelterEmail: email, 
-            shelterEmergencyPhone: emergencyPhone, 
-            shelterDaytimePhone: daytimePhone            
-        })
+            .where('shelterID', thisShelterID)
+            .update({
+                shelterName: name,
+                shelterEmail: email, 
+                shelterEmergencyPhone: emergencyPhone, 
+                shelterDaytimePhone: daytimePhone            
+            })
         .catch(function(err){
-          console.log("Error updating this shetler.", err);
+          console.log("Error updating this shetler", err);
+          throw new Error("Error updating this shetler", err);
         })
         .then(function(updatedShelter){
           console.log("Updated shelter.");
@@ -118,13 +117,13 @@ module.exports.updateShelter = function(req){
 
 module.exports.deleteShelter = function(req){
       var shelter = req;
-      // console.log("Passed in shelter to delete with id", shelter); 
       return knex('shelters')
               .returning('*')
               .where('shelterID', shelter)
               .del()
       .catch(function(err){
         console.log("Something went wrong deleting this shelter");
+        throw new Error("Something went wrong deleting this shelter");
       })
       .then(function(shelter){
         console.log("Deleted shelter with ID ", shelter[0].shelterID);
@@ -137,27 +136,29 @@ module.exports.deleteShelter = function(req){
 module.exports.insertShelterUnit = function(req){
   var unitSize = req.shelterUnit.unitSize;
   return getShelterID(req.shelterName)
-      .then(function(shelterID){
-        console.log("About to insert unit ", unitSize, shelterID);
+      .then(function(shelter){
+        var shelterID = shelter[0].shelterID;
         return knex('shelterUnits')
                 .insert({
                   fk_shelterID: shelterID,
                   unitSize: unitSize
                 })
                 .returning('*')
-                .catch(function(err){
-                  console.log("Something went wrong inserting this shelter unit.", err);
-                })
-                .then(function(shelterUnit){
-                  console.log("Successfully inserted unit ", shelterUnit[0].shelterUnitID);
-                  return shelterUnit;
-                });
+        .catch(function(err){
+          console.log("Something went wrong inserting this shelter unit.", err);
+          throw new Error("Something went wrong inserting this shelter unit.", err);
+        })
+        .then(function(shelterUnit){
+          console.log("Successfully inserted unit ", shelterUnit[0].shelterUnitID);
+          return shelterUnit;
+        });
 
       });
 };
 
 module.exports.deleteShelterUnit = function(req){
   //function for deleting specific shelter unit
+  console.log("REQUEST DATA: ", req);
 };
 
 module.exports.insertShelterEligibility = function(req, shelterID){
