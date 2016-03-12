a45d64a6535f758a6032ea171d93a1fd8761a320
TJ Martin <codeoverdose@gmail.com>
2016-03-12 15:10:38 -0600

    slow start

--- app/components/ShelterList.js
+++ app/components/ShelterList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 import ShelterStore from '../stores/ShelterStore';
 import ShelterActions from '../actions/ShelterActions';
+import Search from './Search';
 
 // extends React.Component no longer binds this like React.createClass, need to do it explicitly
 class Shelter extends React.Component {
   constructor(props) {
     super(props);
 // set initial state
+    this.handleUserInput = this.handleUserInput.bind(this);
     this.state = ShelterStore.getState();
     this.onChange = this.onChange.bind(this);
   }
@@ -28,8 +30,16 @@ class Shelter extends React.Component {
   handleClick(i) {
     console.log('You clicked: ', i);
   }
+
+  handleUserInput(filter, inStockOnly) {
+    this.setState({
+      filterText: filter,
+      womenz: inStockOnly
+    });
+  }
 // shelterProfile is going to be a separate ajax call, utilizing params.id
   render() {
+    console.log('bound to this, it is', this.handleUserInput)
     const shelters = this.state.shelters.map((shelter, i) => {
       const bound = this.handleClick.bind(this, i);
       return (
@@ -44,6 +54,11 @@ class Shelter extends React.Component {
 
     return (
       <div className ="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <SearchBar
+          filter={this.state.filterText}
+          women={this.state.womenz}
+          onInput={this.handleUserInput.bind(this)}
+        />
         <p>List of Shelters</p>
         <ul>
           {shelters}
@@ -52,5 +67,40 @@ class Shelter extends React.Component {
     );
   }
 }
+
+
+class SearchBar extends React.Component {
+  constructor() { 
+    super()
+    console.log(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange() {
+    //console.log(this)
+    this.props.onInput (
+      this.refs.filterTextInput.value,
+      this.refs.gender.checked
+    );
+  }
+  render() {
+    return (
+    <form>
+        <input type="text"
+          placeholder="Search..."
+          value={this.props.filter}
+          ref="filterTextInput"
+          onChange={this.handleChange}
+        />
+        <p>
+          <input type="checkbox" ref="gender" checked={this.props.women} />
+          {' '}
+          Womenz
+        </p>
+    </form>
+    );
+  }
+}
+
+
 
 export default Shelter;
