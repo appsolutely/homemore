29fda90204713fb7cac64bf14ba3fe0544ad5322
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-14 15:37:24 -0500

    updated header

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,61 +3,72 @@ import ReactDOM from 'react-dom'
 import { Link } from 'react-router';
 
 class Header extends React.Component {
-  constructor(){
-    super();
-    this.state = {email: "", password: ""}
-    this.update = this.update.bind(this);
+  render() {
+    return (<div><SignInFields /></div>);
   }
-
-  update(e){
-    this.setState({
-      email: ReactDOM.findDOMNode(this.refs.email.refs.inp).value,
-      password: ReactDOM.findDOMNode(this.refs.password.refs.inp).value
-    })
-  }
-  submitLogin(e){
-    e.preventDefault();
-    let signInInfo = {user: {password: this.state.password, email: this.state.email}}
-
-    console.log("login info! ", signInInfo)
-    this.signIn(signInInfo)
-    this.state = {email: "", password: ""}
-
-  }
-
-  signIn(creds){
-    $.ajax({
-      type: 'POST',
-      url: '/api/signin',
-      data: creds,
-      success: function(data){
-        console.log('success ', data);
-      },
-      fail: function(err){
-        console.log('err', err);
-      }
-  })
 }
 
-render() {
-  return (
-    <div className ="jumbotron col-sm-6 col-sm-offset-3 text-center">
-    <Link to="/">
-      <img className="logo" src="/img/g32644.png" />
-    </Link>
+class SignInFields extends React.Component {
+  constructor(){
+    super()
+      this.state = {email: "", password: ""}
+      this.update = this.update.bind(this);
+    }
 
-        <div>
-          email: <SignInInfo ref = 'email' update={this.update} placeholder="Username"/>
-          password: <SignInInfo ref = 'password' update={this.update} placeholder="Username"/>
-        <button type='submit' onClick={this.submitLogin.bind(this)}>Sign In</button>
-        </div>
-        <Link to="/signup">
-          sign up
-        </Link>
-    </div>
+    update(e){
+      this.setState({
+        email: ReactDOM.findDOMNode(this.refs.email.refs.inp).value,
+        password: ReactDOM.findDOMNode(this.refs.password.refs.inp).value,
+      })
+    }
+    submitLogin(e){
+      e.preventDefault();
+      let signInInfo = {user: {password: this.state.password, email: this.state.email}}
 
-  );
-  }
+      console.log("login info! ", this.state)
+      this.signIn(signInInfo)
+
+    }
+
+    signIn(creds){
+      $.ajax({
+        type: 'POST',
+        url: '/api/signin',
+        data: creds,
+        success: function(data){
+        $( ".loginFields" ).hide();
+        $( ".welcome" ).show();
+        },
+        fail: function(err){
+          console.log('err', err);
+        }
+    })
+    }
+
+    render() {
+    return (
+      <div className ="jumbotron col-sm-6 col-sm-offset-3 text-center">
+      <Link to="/">
+        <img src="/img/sheltered_logo.png" />
+      </Link>
+
+          <div className="loginFields">
+            email: <SignInInfo ref = 'email' update={this.update} placeholder="Username"/>
+            password: <SignInInfo ref = 'password' update={this.update} placeholder="Username"/>
+          <button type='submit' onClick={this.submitLogin.bind(this)}>Sign In</button>
+          <Link to="/signup">
+            sign up
+          </Link>
+          </div>
+          <div className="welcome">
+            <Link to="/user-page">
+              My Account
+            </Link>
+          </div>
+      </div>
+
+    );
+    }
 }
 
 class SignInInfo extends React.Component  {
