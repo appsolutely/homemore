eeb5a72751190b083fc7f77e94728989bacc5272
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-15 15:09:22 -0500

    added approve button

--- app/components/AdminProfile.js
+++ app/components/AdminProfile.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import { Link } from 'react-router';
+import alt from '../alt';
+import UserProfileActions from '../actions/UserProfileActions';
 
 class AdminProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = alt.stores.UserStore.state;
+  }
+  componentDidMount() {
+    //UserStore.listen(this.onChange);
+    UserProfileActions.getUser();
+  }
+
+  approveAccount(){
+    this.post(this.state.userObject.userEmail)
+  }
+
+  post(data){
+    $.ajax({
+      type: 'POST',
+      url: '/api/approve',
+      data:{permission: 'JCB', user: {email: data}},
+      success: function(data){
+        console.log("account has been approved")
+      },
+      fail: function(err){
+        console.log('err', err);
+      }
+    });
+  }
+
   render() {
     return (
-      <div className ="col-sm-6 col-sm-offset-3 text-center">Admin Profile</div>
+      <div>
+      <div className ="col-sm-6 col-sm-offset-3 text-center">
+      <button type='submit' onClick={this.approveAccount.bind(this)}>Approve ME</button>
+      <br />
+      <br />
+      add shelter
+
+       
+      </div>
+      </div>
     );
   }
 }
