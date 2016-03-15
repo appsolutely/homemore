eb01569186d247a1f2d7b124830b77fc8730a646
James Ross <jross102@gmail.com>
2016-03-15 18:48:53 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -405,7 +405,7 @@ app.get('/api/fetchUser', function(req, res){
   //if not logged in will return nothing
   //will only return info about the user that is logged in
   //this is all the info for the profile page -- not any shelter or related info
-  var response = {user: [], shelters: ['tj']};
+  var response = {user: [], shelters: []};
   if (req.session) {
     console.log('inside fetchUser ', req.session.fk_userID);
     return users.findByUserID(req.session.fk_userID)
