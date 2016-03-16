762a88af2bcd2989f21ac2e945affdd84ab38fbb
Julia Brown <juliacelestebrown@gmail.com>
2016-03-16 18:42:56 -0500

    working on passing location through

--- app/components/GoogleMapsView.js
+++ app/components/GoogleMapsView.js
@@ -4,19 +4,28 @@ import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 class ShelterMap extends React.Component {
 
   render() {
+    const shelterLoc = this.props.shelters;
+    console.log()
     return (
       <div className="map" style={{ height: '300px', border: '1px black solid' }}>
          <GoogleMapLoader
            ref={(map) => { console.log(map); }}
            containerElement={ <div style={{ height: '100%' }} /> }
            googleMapElement={
-            <GoogleMap defaultZoom={14} defaultCenter={{ lat: 30.2672, lng: -97.7431 }}>
-            </GoogleMap>
+            <GoogleMap
+              defaultZoom={18}
+              defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+            />
           }
          />
+         {{ shelterLoc }}
          </div>
       );
   }
 }
+
+ShelterMap.propTypes = {
+  shelters: React.PropTypes.array,
+};
 
 export default ShelterMap;
--- app/components/ShelterProfile.js
+++ app/components/ShelterProfile.js
@@ -20,7 +20,9 @@ class ShelterProfile extends React.Component {
          <div className="shelterProfile">
           <h3>{theShelter.shelterName}</h3>
           <h3>{theShelter.shelterDaytimePhone}</h3>
-          <ShelterMap />
+          <ShelterMap
+            shelters={theShelter}
+          />
           <h3>Hours</h3>
           <table>
             <tbody>
