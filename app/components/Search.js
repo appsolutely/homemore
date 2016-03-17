3b6bdc3075a1c4e1392ef9827a71a38e7efc3f8e
TJ Martin <codeoverdose@gmail.com>
2016-03-17 11:23:45 -0500

    minor bugfix - submitting PR w/revisions for Carly

--- app/components/Search.js
+++ app/components/Search.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
     this.props.onInput(
       this.refs.filterTextInput.value,
       this.refs.gender.checked,
-      this.refs.family.checked,
+      this.refs.family.checked
     );
   }
   render() {
