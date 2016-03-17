b21ec27242aafb7b8c20776b53a81cd90d175bdc
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-16 22:23:51 -0500

    removed ShelterMap as it was breaking shelter profile

--- app/components/ShelterProfile.js
+++ app/components/ShelterProfile.js
@@ -20,9 +20,6 @@ class ShelterProfile extends React.Component {
          <div className="shelterProfile">
           <h3>{theShelter.shelterName}</h3>
           <h3>{theShelter.shelterDaytimePhone}</h3>
-          <ShelterMap
-            shelters={theShelter}
-          />
           <h3>Hours</h3>
           <table>
             <tbody>
