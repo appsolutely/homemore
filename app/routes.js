3ff93bdcf101720c5ea5e8ac0fc0d04e92f92e9c
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-21 17:40:21 -0500

    removed unused routes

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,7 @@ import { Route } from 'react-router';
 import App from './components/App';
 import ShelterList from './components/ShelterList';
 import ShelterProfile from './components/ShelterProfile';
-import Signup from './components/Signup';
 import AdminSignup from './components/AdminSignup';
-import UserSignup from './components/UserSignup';
 import AdminProfile from './components/AdminProfile';
 import UserProfile from './components/UserProfile';
 import ManagerProfile from './components/ManagerProfile';
@@ -16,8 +14,6 @@ export default (
     <Route path="/" component={ShelterList} />
     <Route path="/shelter" component={ShelterList} />
     <Route path="/shelter/:id" component={ShelterProfile} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/user-signup" component={UserSignup} />
     <Route path="/admin-signup" component={AdminSignup} />
     <Route path="/admin-profile" component={AdminProfile} />
     <Route path="/manager-profile" component={ManagerProfile} />
