11cc8272cabbaecdffa2cedae50c38069b0cfd84
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-21 12:54:38 -0500

    state is not returning correctly for state

--- app/components/Occupy.js
+++ app/components/Occupy.js
@@ -1,34 +1,22 @@
 import React from 'react'
+import OccpancyAction from '../actions/OccpancyActions';
+import OccupancyStore from '../stores/OccupancyStore';
 
 class Occupy extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {items: [], task: ''}
+    this.state = OccupancyStore.getState();
     this.onChange = this.onChange.bind(this);
-    this.addTask = this.addTask.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
+    this.addOccupent = this.addOccupent.bind(this);
+    this.deleteOccupent = this.deleteOccupent.bind(this);
   }
 
-  deleteTask(e) {
+  deleteOccupent(e) {
     var taskIndex = parseInt(e.target.value, 10);
-    console.log('remove task: %d', taskIndex, this.state.items[taskIndex]);
+    console.log('removed occupant: %d', taskIndex, this.state.items[taskIndex]);
     this.setState(state => {
       state.items.splice(taskIndex, 1);
       return {items: state.items};
-    });
-  }
-
-  stateStuff(){
-    $.ajax({
-      type: 'POST',
-      url: '/api/fetchShelterOccupants',
-      data:{shelters: {shelterName: 'ARCH Emergency Night Shelter'}},
-      success: function(data){
-        console.log("getting Occupents for Men Emergency Night Shelter", data)
-      },
-      fail: function(err){
-        console.log('err', err);
-      }
     });
   }
 
@@ -36,56 +24,56 @@ class Occupy extends React.Component {
     this.setState({ task: e.target.value });
   }
 
-  addTask(e){
+  addOccupent(e){
     e.preventDefault();
-    this.setState({
-      items: this.state.items.concat([this.state.task]),
-      task: ''
-    })
-    this.stateStuff()
+    console.log(this.state)
+    // this.setState({
+    //   items: this.state.items.concat([this.state.task]),
+    //   task: ''
+    // })
+    // this.stateStuff()
   }
-
   render(){
     return(
       <div className ="col-sm-6 col-sm-offset-3 text-center">
         <h1 >Add occupants</h1>
 
-          <form onSubmit={this.addTask}>
+          <form onSubmit={this.addOccupent}>
             <input onChange={this.onChange} type="text" value={this.state.task}/>
             <button> Add </button>
             </form>
-            <List items={this.state.items} deleteTask={this.deleteTask} />
           </div>
         );
       }
     }
 
+    //<List items={this.state.items} deleteOccupent={this.deleteOccupent} />
 
-class List extends React.Component {
-  deleteElement(){
-      console.log("remove");
-  }
-
-  render(){
-
-      var displayTask  = function(task, taskIndex){
-
-
-          return <li>
-              {task}
-              <button onClick= {this.deleteElement}> Delete </button>
-          </li>;
-      };
-
-      return <ul>
-          {this.props.items.map((task, taskIndex) =>
-              <li key={taskIndex}>
-                  {task}
-                  <button onClick={this.props.deleteTask} value={taskIndex}> Delete </button>
-              </li>
-          )}
-      </ul>;
-  }
-}
+// class List extends React.Component {
+//   deleteElement(){
+//       console.log("remove");
+//   }
+//
+//   render(){
+//
+//       var displayTask  = function(task, taskIndex){
+//
+//
+//           return <li>
+//               {task}
+//               <button onClick= {this.deleteElement}> Delete </button>
+//           </li>;
+//       };
+//
+//       return <ul>
+//           {this.props.items.map((task, taskIndex) =>
+//               <li key={taskIndex}>
+//                   {task}
+//                   <button onClick={this.props.deleteOccupent} value={taskIndex}> Delete </button>
+//               </li>
+//           )}
+//       </ul>;
+//   }
+// }
 
 export default Occupy;
