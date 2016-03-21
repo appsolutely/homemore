3f4be39116346d5e1724628da7b67b607d40a25a
James Ross <James@Jamess-MacBook-Pro-2.local>
2016-03-21 16:05:03 -0500

    updated routes to prevent page not found

--- app/components/ShelterListings.js
+++ app/components/ShelterListings.js
@@ -13,14 +13,14 @@ class ShelterListings extends React.Component {
       if (shelter.shelterName.toLowerCase().indexOf(this.props.filter.toLowerCase()) >= 0) {
         return (
           <div key={shelter.shelterID} className="well shelterCard">
-						<Link to={'/shelterprofile/' + shelter.shelterID}>                            
+						<Link to={'/shelter/' + shelter.shelterID}>                            
               <div className="text-left shelterInfo">
                   <div className="org text-capitalize"><p><b>{shelter.organizationName}</b></p></div>
                   <h4><span className="shelterName"><b>{shelter.shelterName}</b></span></h4>
-                  <h5 className="text-lowercase"> operated by 
+                  <h5 className="text-lowercase"> operated by
                     <span className="orgName text-capitalize">
                       <em> {shelter.organizationName}</em>
-                    </span> @ the 
+                    </span> @ the
                     <span className="locationName text-capitalize"> {shelter.locationName}
                     </span>
                   </h5>
