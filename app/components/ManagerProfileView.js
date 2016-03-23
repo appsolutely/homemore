20984b7ce861c53ba2cfd57ab89c13094c52e1ac
Carly Levy <carlylevy@gmail.com>
2016-03-23 17:02:01 -0500

    styling

--- app/components/ManagerProfileView.js
+++ app/components/ManagerProfileView.js
@@ -16,19 +16,21 @@ class ManagerProfileView extends React.Component {
 
   render(){
     return (
-      <div>
-        <div>orgName: {this.props.shelterInfo.organizationName}</div>
+      <div className="well text-left">
+        <h2>Manage {this.props.shelterInfo.shelterName}</h2>
+        <div><h3>Basic Infomation</h3></div>
+        <div>Organization Name: {this.props.shelterInfo.organizationName}</div>
         <div>Shelter Name: {this.props.shelterInfo.shelterName}</div>
         <div>Daytime Phone: {this.props.shelterInfo.shelterDaytimePhone}</div>
         <div>Emergency Phone: {this.props.shelterInfo.shelterEmergencyPhone}</div>
         <div>Email: {this.props.shelterInfo.shelterEmail}</div>
-        <div>location Information</div>
+        <div><h3>Location Details</h3></div>
         <div>Location name: {this.props.shelterInfo.locationName}</div>
         <div>Street Address: {this.props.shelterInfo.locationStreet}</div>
         <div>City: {this.props.shelterInfo.locationCity}</div>
         <div>State: {this.props.shelterInfo.locationState}</div>
         <div>Zip: {this.props.shelterInfo.locationZip}</div>
-        <div>hours</div>
+        <div><h3>Hours of Operation</h3></div>
         <div>Monday: {this.props.shelterInfo.hoursMonday}</div>
         <div>Tuesday: {this.props.shelterInfo.hoursTuesday}</div>
         <div>Wednesday: {this.props.shelterInfo.hoursWednesday}</div>
@@ -36,7 +38,7 @@ class ManagerProfileView extends React.Component {
         <div>Friday: {this.props.shelterInfo.hoursFriday}</div>
         <div>Saturday: {this.props.shelterInfo.hoursSaturday}</div>
         <div>Sunday: {this.props.shelterInfo.hoursSunday}</div>
-        <button onClick={this.handleClick}>Edit Me</button>
+        <button className="btn btn-primary" onClick={this.handleClick}>Edit Me</button>
       </div>
     )
   }
