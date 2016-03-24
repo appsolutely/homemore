f6a11979a27090287e438cd50b1d8356bb87ed46
Julia Brown <juliacelestebrown@gmail.com>
2016-03-24 15:54:47 -0500

    update user

--- app/components/AdminSignup.js
+++ app/components/AdminSignup.js
@@ -20,7 +20,6 @@ const SignUpForm = React.createClass({
             </Formsy.Form>
         );
     }
-
 });
 
 
--- app/components/UserProfileEdit.js
+++ app/components/UserProfileEdit.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router';
 import alt from '../alt';
+import Formsy from 'formsy-react';
+import FRC from 'formsy-react-components';
+
+const {Input} = FRC;
+
+const UpdateUserForm = React.createClass({
+    mixins: [FRC.ParentContextMixin],
+    render() {
+        return (
+            <Formsy.Form
+                {...this.props}
+                ref="formsy"
+            >
+                {this.props.children}
+            </Formsy.Form>
+        );
+    }
+});
 
 class UserProfileEdit extends React.Component {
 	constructor(props) {
@@ -10,12 +28,12 @@ class UserProfileEdit extends React.Component {
 
   handleClick(e) {
     e.preventDefault();
-    var first = this.refs.firstName.value || this.props.userInfo.userFirstName;
-    var last = this.refs.lastName.value || this.props.userInfo.userLastName;
-    var email = this.refs.email.value || this.props.userInfo.userEmail;
-    var password = this.refs.password.value;
-    var phone = this.refs.phone.value || this.props.userInfo.userPhone;
-    var flag = this.refs.password.value.length === 0 ? false : true;
+    var first = this.refs.firstName.getValue() || this.props.userInfo.userFirstName;
+    var last = this.refs.lastName.getValue() || this.props.userInfo.userLastName;
+    var email = this.refs.email.getValue() || this.props.userInfo.userEmail;
+    var password = this.refs.password.getValue();
+    var phone = this.refs.phone.getValue() || this.props.userInfo.userPhone;
+    var flag = this.refs.password.getValue().length === 0 ? false : true;
     this.props.save(
       first,
       last,
@@ -31,29 +49,65 @@ class UserProfileEdit extends React.Component {
 
   render() {
     return (
-      <form className="text-left">
-        <div>
-          <label>First Name</label>
-          <input type='text' ref='firstName' placeholder={this.props.userInfo.userFirstName} />
-        </div>
-        <div>
-          <label>Last Name</label>
-          <input type='text' ref='lastName' placeholder={this.props.userInfo.userLastName} />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type='email' ref='email' placeholder={this.props.userInfo.userEmail} />
-        </div>
-        <div>
-          <label>Password</label>
-            <input type='password' ref='password' size='6' />
-        </div>
-        <div>
-          <label>Phone</label>
-          <input type='tel' ref='phone' placeholder={this.props.userInfo.userPhone} />
-        </div>
+      <UpdateUserForm
+        onValidSubmit={this.submitForm}
+        className="text-left">
+          <Input
+            ref="email"
+            onUpdate={this.update}
+            name="email"
+            value=""
+            label="Email"
+            type="email"
+            placeholder="Email"
+            validations="isEmail"
+            validationErrors={{
+                isEmail: 'This doesn’t look like a valid email address.'
+            }}
+          />
+          <Input
+            onUpdate={this.update}
+            ref="password"
+            name="password1"
+            value=""
+            label="Password"
+            type="password"
+            validations="minLength:8"
+            validationError="Your password must be at least 8 characters long."
+            placeholder="Choose a password"
+          />
+          <Input
+            onUpdate={this.update}
+            ref="firstName"
+            name="firstName"
+            value=""
+            label="First Name"
+            type="text"
+            placeholder="First Name"
+          />
+          <Input
+            onUpdate={this.update}
+            ref="lastName"
+            name="lastName"
+            value=""
+            label="Last Name"
+            type="text"
+            placeholder="Last Name"
+          />
+          <Input
+            onUpdate={this.update}
+            ref="phone"
+            name="phone"
+            value=""
+            label="Phone Number"
+            validations={{isLength: 10,
+                          isNumeric: true}}
+            validationError="Must be a valid phone number"
+            type="text"
+            placeholder="Phone Number"
+          />
         <button className="btn btn-primary editButton" onClick={this.handleClick} >Save Changes</button>
-      </form>
+      </UpdateUserForm>
     );
   }
 }
