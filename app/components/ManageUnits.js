82131c9abe63982cd24a1d41681f2cf39ff1d9aa
Julia Brown <juliacelestebrown@gmail.com>
2016-03-24 19:01:44 -0500

    unit forms updated

--- app/components/ManageUnits.js
+++ app/components/ManageUnits.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
 import alt from '../alt';
+import Formsy from 'formsy-react';
+import FRC from 'formsy-react-components';
+
+
+const {Input, Select} = FRC;
+
+const NewUnitForm = React.createClass({
+    mixins: [FRC.ParentContextMixin],
+
+    render() {
+        return (
+            <Formsy.Form
+                {...this.props}
+                ref="formsy"
+            >
+                {this.props.children}
+            </Formsy.Form>
+        );
+    }
+});
 
 class ManageUnits extends React.Component {
   constructor(props) {
@@ -11,14 +31,14 @@ class ManageUnits extends React.Component {
 // stuff
   
   addUnits(e){
-    e.preventDefault();
     console.log('adding a unit without click')
     const theShelter = this.props.shelter;
     console.log(theShelter);
     //clean up everything that isn't a number
-    const theBeds = this.refs.beds.value.split('').filter((num) => parseInt(num));
+    const beds = this.refs.beds.getValue()
+    const theBeds = beds.split('').filter((num) => parseInt(num));
     theBeds.push('BD');
-    const unit = {shelterUnit: {unitSize: theBeds.join('')}, 
+    const unit = {shelterUnit: {unitSize: theBeds.join(''), unitName: this.refs.unitName.getValue()}, 
     shelters: {shelterName: theShelter.shelterName}, 
     organizations: {orgName: theShelter.organizationName}};
     this.props.add(unit);
@@ -26,27 +46,47 @@ class ManageUnits extends React.Component {
   }
 
   removeUnits(e){
-    e.preventDefault();
+    // e.preventDefault();
     //need to pass it shelterUnitID
     ManagerActions.removeUnits();
   }
 
 
   render(){
+    const bedSelect = [
+            {value: '1', label: '1BD'},
+            {value: '2', label: '2BD'},
+            {value: '3', label: '3BD'}
+        ];
+
+
     return (
       <div>
-        <form className="well form-inline">
+        <NewUnitForm 
+        onValidSubmit={this.addUnits}
+        className="form-inline"
+        >
         <span><h3>Add Units</h3></span>
-          <span className="selectUnits"><span>Beds per unit: </span>
-              <select className="form-control selectWidth" ref='beds'>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </select>
+          <span className="selectUnits">
+            <Select
+              ref="beds"
+              name="beds"
+              label="Beds per unit"
+              options={bedSelect}
+              required
+            />
+              <Input
+                ref="unitName"
+                name="unitName"
+                value=""
+                label="Unit Name"
+                type="text"
+                placeholder="Unit Name..."
+                required
+              />
           </span>
-          <input type="text" ref=''></input>
-          <button className="btn btn-primary btn-sm editButton" onClick={this.addUnits}>Add Unit</button>
-        </form>
+          <button className="btn btn-primary btn-sm editButton" onValidSubmit={this.addUnits}>Add Unit</button>
+        </NewUnitForm>
       </div>
     );
   }
